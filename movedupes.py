b255f4e1b7be7b63dd6743d12b4a92e244576921
redrubberband <thedarkfox01@gmail.com>
2020-07-07 06:03:06 +0700

    added comments

--- movedupes.py
+++ movedupes.py
@@ -9,14 +9,22 @@ extensions_to_check = [
 ]
 
 def main():
+    
     files_in_dir = getAllFiles()
-
+    isDuplicateFound = False
+    
+    print("Checking for duplicates...")
     for file in files_in_dir:
-
         if file.is_file():
             filename = file.stem
             if any(extension in filename for extension in extensions_to_check):
                 move(file, DUPLICATE_DIRECTORY)
+                isDuplicateFound = True
+
+    if (isDuplicateFound):
+        print("Duplicates found and successfuly moved!")
+    else:
+        print("No duplicate found!")
 
 main()
 
